214ddf93928c6c1d114c7449f22dc3e10250a9db
ReeceC <reececrook16@gmail.com>
2023-08-19 15:02:39 -0600

    Completed Lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr.Cholocate", 8, "M", "brown", "calm", 1),
+("Rowdy", 3, "M", "tan", "rowdy", 1),
+("Tabitha", 8, "F", "black", "friendly", 1),
+("Sergeant Brown", 8, "M", "brown", "Calm", 1),
+("Melissa", 8, "F", "brown", "friendly", 1),
+("Grinch", 8, "M", "black", "grumpy", 1),
+("Wendy", 8, "F", "brown", "playful", 1),
+(NULL, NULL, "F", "grey", "timid", 1);
